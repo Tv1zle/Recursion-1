87a663ee6b5c9daf063809f3c803484f53a3d889
User <User@WIN-D6I7BGDFHRA>
2022-10-10 15:43:29 +0200

    Добавьте файлы проекта.

---
+++ HomeWork24.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+
+int Fibonachi(int number)
+{
+    if (number == 0)
+    {
+        return 0;
+    }
+    else if (number == 1)
+    {
+        return 1;
+    }
+
+
+    return Fibonachi(number - 1) + Fibonachi(number - 2);
+}
+
+
+int main()
+{
+    int number;
+    std::cin >> number;
+
+
+    std::cout << Fibonachi(number);
+}
